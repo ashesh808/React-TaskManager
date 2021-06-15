5192a92e5b07f1c8e83c505a9f64e622957445c5
ashesh808 <asheshusa9@gmail.com>
2021-06-15 14:09:38 +0545

    Added Toogle Add Feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddTask from './components/AddTask'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([{
       id: 1,
       text: 'IELTS',
@@ -50,8 +51,8 @@ function App() {
   return (
 
     <div className = "container" >
-    <Header title = "Task Tracker"/>
-    <AddTask onAdd= {addTask}/>
+    <Header title = "Task Tracker" onAdd = {() => setShowAddTask (!showAddTask)} showAdd= {showAddTask}/>
+    {showAddTask && <AddTask onAdd= {addTask}/>}
 
     {tasks.length > 0 ? (<Tasks tasks = {tasks}  onDelete= {deleteTask} onToggle= {toogleRemainder}/>) : ('No task to show here') }
     </div>
